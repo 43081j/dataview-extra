94bc8974a382f15205fd8730ac2c29412c66250c
Lennart Melzer <me@lmaa.name>
2013-11-12 22:11:55 +0100

    [fix] encoding issues with getString

--- dist/dataview-extra.js
+++ dist/dataview-extra.js
@@ -3,27 +3,62 @@
  * 43081j
  * License: MIT, see LICENSE
  */
-DataView.prototype.getString = function(length, offset, raw) {
+DataView.decodeUtf8 = function decodeUtf8(arrayBuffer) {
+  var result = "";
+  var i = 0;
+  var c = 0;
+  var c1 = 0;
+  var c2 = 0;
+
+  var data = new Uint8Array(arrayBuffer);
+
+  // If we have a BOM skip it
+  if (data.length >= 3 && data[0] === 0xef && data[1] === 0xbb && data[2] === 0xbf) {
+    i = 3;
+  }
+
+  while (i < data.length) {
+    c = data[i];
+
+    if (c < 128) {
+      result += String.fromCharCode(c);
+      i++;
+    } else if (c > 191 && c < 224) {
+      if (i + 1 >= data.length) {
+        throw "UTF-8 Decode failed. Two byte character was truncated.";
+      }
+      c2 = data[i + 1];
+      result += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+      i += 2;
+    } else {
+      if (i + 2 >= data.length) {
+        throw "UTF-8 Decode failed. Multi byte character was truncated.";
+      }
+      c2 = data[i + 1];
+      c3 = data[i + 2];
+      result += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+      i += 3;
+    }
+  }
+  return result;
+}
+
+
+DataView.prototype.getString = function(length, offset) {
 	offset = offset || 0;
 	length = length || (this.byteLength - offset);
 	if(length < 0) {
 		length += this.byteLength;
 	}
-	var str = '';
+	var data = [];
+	for(var i = offset; i < (offset + length); i++) {
+		data.push(this.getUint8(i));
+	}
+
 	if(typeof Buffer !== 'undefined') {
-		var data = [];
-		for(var i = offset; i < (offset + length); i++) {
-			data.push(this.getUint8(i));
-		}
 		return (new Buffer(data)).toString();
 	} else {
-		for(var i = offset; i < (offset + length); i++) {
-			str += String.fromCharCode(this.getUint8(i));
-		}
-		if(raw) {
-			return str;
-		}
-		return decodeURIComponent(escape(str));
+		return DataView.decodeUtf8(data);
 	}
 };
 
